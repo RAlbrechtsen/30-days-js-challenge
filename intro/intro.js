5a4052e934db5a9e10fc72b02329dedac2343a0c
unknown <r.albrechtsen93@gmail.com>
2023-03-09 10:56:19 +0100

    first day of js

---
+++ intro/intro.js
@@ -0,0 +1,45 @@
+/**const a = "Hello";
+const b = "World!";
+const c = "This is javascript";
+
+console.log(a, b, c);
+
+console.log(2 + 3);
+console.log(3 - 2);
+console.log(2 * 3);
+console.log(3 / 2);
+console.log(3 % 2);
+console.log(3 ** 2);
+
+
+*/ 
+
+// let firstName;
+// console.log(firstName);
+
+// let lastName = null;
+// console.log(lastName);
+
+// console.log(typeof 'Rene'); // string
+// console.log(typeof 5); // integer
+// console.log(typeof true); // boolean
+// console.log(typeof null); // object type
+// console.log(typeof undefined); // undefined
+
+// let firstName = 'Rene';
+// let lastName = 'Albrechtsen';
+// let country = 'Denmark';
+// let city = 'Taastrup';
+// let age = 29;
+// let isMarried = false;
+
+// console.log(firstName, lastName, country, city, age, isMarried);
+
+let age = 100 //age in years
+const gravity = 9.81 // earth gravity in m/s2
+const boilingPoint = 100 // water boiling point in celsius
+const PI = 3.14 // geometrical constant
+console.log(gravity, boilingPoint, PI);
+
+let firstName = 'Rene', occupation = 'student', live = 'Denmark';
+console.log(firstName, occupation, live);
